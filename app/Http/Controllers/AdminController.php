3b1a84ed38f45553401e89f1df92d1ac69d4e4c5
UsamaUmer109 <66095107+UsamaUmer109@users.noreply.github.com>
2023-11-26 14:06:50 +0500

    Make Admin Dashboard, show list of users, categories and products . Also give right to delete
    Make Admin Dashboard, show list of users, categories and products . Also give right to delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Product;
 
 class AdminController extends Controller
 {
+    // for check auth
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    // show admin dashboard
     public function dashboard(Request $request)
     {
-        return view('auth.register');
+        return view('admin.dashboard');
     }
+    // show admin user list
+    public function users(Request $request)
+    {
+        $users = User::where('role', '!=', 'admin')->paginate(10);
+        return view('admin.users', compact('users'));
+    }
+    // delete user from admin viewing list
+    public function deleteUser($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return redirect()->route('admin.users')->with('error', 'User not found...!!!');
+        }
+        $user->delete();
+            return redirect()->route('admin.users')->with('success', 'User deleted successfully..!!');
+    }
+    // Show categories List
+    public function categories(Request $request)
+    {
+        $categories = Category::select('*')->paginate(10);
+        return view('admin.categories', compact('categories'));
+    }
+    // Show Product List
+    public function products(Request $request)
+    {
+        $categories = Category::select('*')->get();
+        $products = Product::select('*')->paginate(10);
+        return view('admin.products', compact('products', 'categories'));
+    }
+    // delete product from admin viewing list
+    public function deleteProduct($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return redirect()->route('admin.products')->with('error', 'Product not found...!!!');
+        }
+        $product->delete();
+            return redirect()->route('admin.products')->with('success', 'Product deleted successfully..!!');
+    }
+
 }
