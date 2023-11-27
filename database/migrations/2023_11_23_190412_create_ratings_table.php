e0b9a8541cc8c7188861f11f18524fbcbff9e49f
Usama Umer <66095107+UsamaUmer109@users.noreply.github.com>
2023-11-28 00:51:19 +0500

    seeder files
    seeder files

--- database/migrations/2023_11_23_190412_create_ratings_table.php
+++ database/migrations/2023_11_23_190412_create_ratings_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->integer('user_id');
             $table->integer('product_id');
+            $table->text('title')->nullable();
+            $table->text('category')->nullable();
             $table->text('review')->nullable();
             $table->integer('rating');
             $table->tinyInteger('status');
--- database/seeders/RatingsTableSeeder.php
+++ database/seeders/RatingsTableSeeder.php
@@ -14,15 +14,15 @@ class RatingsTableSeeder extends Seeder
     public function run(): void
     {
         $rating = [
-            [ 'id' => 1, 'user_id' => 1,  'product_id' => 1, 'review' =>  'Very Good Taste','rating' =>  5,   'status' => 0],
-            [ 'id' => 2, 'user_id' => 3,  'product_id' => 2, 'review' =>  'I like your taste','rating' =>  5,   'status' => 0],
-            [ 'id' => 3, 'user_id' => 3,  'product_id' => 1, 'review' =>  'Very Good Taste','rating' =>  4,   'status' => 0],
-            [ 'id' => 4, 'user_id' => 4,  'product_id' => 2, 'review' =>  'Very Good Taste','rating' =>  4,   'status' => 0],
-            [ 'id' => 5, 'user_id' => 7,  'product_id' => 3, 'review' =>  'Average Taste','rating' =>  3,   'status' => 0],
-            [ 'id' => 6, 'user_id' => 6,  'product_id' => 3, 'review' =>  'Very Bad Taste','rating' =>  2,   'status' => 0],
-            [ 'id' => 7, 'user_id' => 5,  'product_id' => 4, 'review' =>  'Amazing Taste','rating' =>  4,   'status' => 0],
-            [ 'id' => 8, 'user_id' => 3,  'product_id' => 4, 'review' =>  'Very Marvelous ','rating' =>  5,   'status' => 0],
-            [ 'id' => 9, 'user_id' => 2,  'product_id' => 5, 'review' =>  'Taste is okay','rating' =>  3,   'status' => 0],
+            [ 'id' => 1, 'user_id' => 1,  'product_id' => 1, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Very Good Taste','rating' =>  5,     'status' => 1],
+            [ 'id' => 2, 'user_id' => 3,  'product_id' => 2, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'I like your taste','rating' =>  5,   'status' => 1],
+            [ 'id' => 3, 'user_id' => 3,  'product_id' => 1, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Very Good Taste','rating' =>  4,     'status' => 1],
+            [ 'id' => 4, 'user_id' => 4,  'product_id' => 2, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Very Good Taste','rating' =>  4,     'status' => 1],
+            [ 'id' => 5, 'user_id' => 7,  'product_id' => 3, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Average Taste','rating' =>  3,       'status' => 1],
+            [ 'id' => 6, 'user_id' => 6,  'product_id' => 3, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Very Bad Taste','rating' =>  2,      'status' => 1],
+            [ 'id' => 7, 'user_id' => 5,  'product_id' => 4, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Amazing Taste','rating' =>  4,       'status' => 1],
+            [ 'id' => 8, 'user_id' => 3,  'product_id' => 4, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Very Marvelous ','rating' =>  5,     'status' => 1],
+            [ 'id' => 9, 'user_id' => 2,  'product_id' => 5, 'title' => 'About Taste', 'category' => 'taste', 'review' =>  'Taste is okay','rating' =>  3,       'status' => 1],
 
         ];
         Rating::insert($rating);
